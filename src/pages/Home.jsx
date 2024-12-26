caddf7e6cf2a0d756509dbd81df861d0dd8333ea
Moniruzzaman-Monir <ad.monir2001@gmail.com>
2024-12-26 12:53:26 +0600

    Find Your Exclusive Offer section done with awesome animation.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,15 @@ import { TbBrandBooking } from 'react-icons/tb';
 import { Link } from 'react-router-dom';
 import FeaturedCar from '../components/FeaturedCar';
 import Testimonial from '../components/Testimonial';
+import { AnimatePresence, motion } from 'framer-motion';
+import Coupon from '../components/Coupon';
 
 const Home = () => {
   const [cars, setCars] = useState([]);
   const [testimonials, setTestimonials] = useState([]);
+  const [coupons, setCoupons] = useState([]);
 
-
+  // fetch featured cars
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/featuredCars`)
       .then((response) => response.json())
@@ -21,11 +24,22 @@ const Home = () => {
       .catch((error) => console.error('Error:', error));
   }, []);
 
+  // fetch testimonial data
   useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/testimonials`)
       .then((response) => response.json())
       .then((data) => {
         setTestimonials(data);
+      })
+      .catch((error) => console.error('Error:', error));
+  }, []);
+
+  // fetch coupons data
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/coupons`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCoupons(data);
       })
       .catch((error) => console.error('Error:', error));
   }, []);
@@ -115,10 +129,10 @@ const Home = () => {
       {/* Recent listing */}
       <div className="w-11/12 mx-auto my-14">
         <div className="text-center space-y-1 mb-14">
-          <p className="font-body text-[#ff3600] ">
+          <p className="font-body italic text-gray-500 ">
             Find the Ride That Drives You Forward!
           </p>
-          <h1 className="font-heading text-3xl font-semibold ">
+          <h1 className="font-heading text-[#ff3600] font-semibold text-3xl ">
             Discover Your Dream Car Today!
           </h1>
         </div>
@@ -131,21 +145,59 @@ const Home = () => {
       </div>
 
       {/* testimonials */}
-      <div className=''>
+      <div className="">
         <div className="text-center my-14 space-y-2">
-          <p className="font-body text-lg text-[#ff3600]">
+          <p className="font-body italic text-gray-500">
             Voices of Trust, Stories of Satisfaction!
           </p>
-          <h1 className="font-heading text-3xl font-semibold">
+          <h1 className="font-heading text-[#ff3600] font-semibold text-3xl">
             What Our Clients are Said About Our Service
           </h1>
         </div>
-        <div>
+        <div className="w-11/12 mx-auto flex gap-5 my-10">
+        {/* <AnimatePresence>
+  {show ? <motion.div exit={{ opacity: 0 }} /> : null}
+</AnimatePresence> */}
           {testimonials.map((testimonial) => (
             <Testimonial
               key={testimonial._id}
               testimonial={testimonial}
             ></Testimonial>
+          ))}
+          {/* <motion.div
+            className="flex gap-5"
+            animate={{
+              x: 0,
+            }}
+            initial={{ x: '100%' }}
+            exit={{ x: '-100%' }}
+            transition={{
+              duration: 0.5,
+              ease: 'easeInOut',
+            }}
+          >
+            {testimonials.map((testimonial) => (
+              <motion.div key={testimonial._id} className="min-w-full">
+                <Testimonial testimonial={testimonial} />
+              </motion.div>
+            ))}
+          </motion.div> */}
+        </div>
+      </div>
+
+      {/* special offers */}
+      <div className="w-11/12 mx-auto ">
+        <div className="text-center pt-14">
+          <p className="font-body italic text-gray-500">
+            Unlock the Joy of Driving!
+          </p>
+          <h1 className="font-heading text-[#ff3600] font-semibold text-3xl">
+            Find Your Exclusive Offer
+          </h1>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6  my-14">
+          {coupons.map((coupon) => (
+            <Coupon key={coupon._id} coupon={coupon}></Coupon>
           ))}
         </div>
       </div>
